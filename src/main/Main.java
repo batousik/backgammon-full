431c215c4f75c537c62140af379cc0e191c0b40e
jn34 <jn34@pc2-012-l.cs.st-andrews.ac.uk>
2014-04-30 16:53:20 +0100

    new main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,12 @@
 package main;
 
+import gui.MainWindow;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MainWindow mv = new MainWindow();
 	}
 
 }
